4014b158dd5e13b8cab8b30815bad29f41346f66
TimJoePat <TimJoePat88@cox.net>
2024-01-29 12:33:55 -0700

    196 Complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 import { useEffect, useReducer } from "react";
 
 const initialState = {
@@ -44,6 +45,8 @@ function reducer(state, action) {
             ? state.points + question.points
             : state.points,
       };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
     default:
       throw new Error("reducer Action unknown");
   }
@@ -76,11 +79,17 @@ export default function App() {
           />
         )}
         {status === "active" && (
-          <Question
-            question={questions[index]}
-            dispatch={dispatch}
-            answer={answer}
-          />
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+            />
+          </>
         )}
       </Main>
     </div>
