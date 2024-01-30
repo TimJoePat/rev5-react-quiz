8fe398b90c06746af33266d712d29a168ec00228
TimJoePat <TimJoePat88@cox.net>
2024-01-30 11:36:34 -0700

    in 197 - finish button works

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import { useEffect, useReducer } from "react";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -48,6 +49,8 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return { ...state, status: "finished" };
     default:
       throw new Error("reducer Action unknown");
   }
@@ -100,8 +103,16 @@ export default function App() {
             <NextButton
               dispatch={dispatch}
               answer={answer}
+              numQuestions={numQuestions}
+              index={index}
             />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+          />
         )}
       </Main>
     </div>
